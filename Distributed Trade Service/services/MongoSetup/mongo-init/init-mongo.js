28377edbe15d3e3004f4c9f973d9f347caeb0a89
isaacgutt <75759022+isaacgutt@users.noreply.github.com>
2024-04-08 12:08:16 -0400

    Update init-mongo.js

--- Distributed Trade Service/services/MongoSetup/mongo-init/init-mongo.js
+++ Distributed Trade Service/services/MongoSetup/mongo-init/init-mongo.js
@@ -1,13 +1,3 @@
-// Switch to CommandDB and create collections and insert documents
-db = db.getSiblingDB('CommandDB');
-
-db.createCollection("clientPortfolio");
-db.clientPortfolio.insertOne({ clientID: "exampleClientId", stockID: "exampleStockId", quantityOwned: 11, timestamp: new Date(), lastUpdated: new Date() });
-
-
-db.createCollection("transactions");
-db.transactions.insertOne({ txnID: "exampleTxnIdYY", clientID: "exampleClientId", stockID: "exampleStockId", quantity: 5, buySell: "buy", price: 150.60, timestamp: new Date(), lastUpdated: new Date() });
-
 // Switch to QueryDB and do the same
 db = db.getSiblingDB('QueryDB');
 

f60d9a05c05d9f1495b78e730e04c2f4b25373ae
isaacgutt <75759022+isaacgutt@users.noreply.github.com>
2024-04-08 22:22:42 -0400

    Delete Distributed Trade Service/services/MongoSetup/mongo-init directory

--- Distributed Trade Service/services/MongoSetup/mongo-init/init-mongo.js
+++
@@ -1,9 +0,0 @@
-// Switch to QueryDB and do the same
-db = db.getSiblingDB('QueryDB');
-
-db.createCollection("clientPortfolio");
-db.clientPortfolio.insertOne({ clientID: "exampleClientId", stockID: "exampleStockId", quantityOwned: 11, timestamp: new Date(), lastUpdated: new Date() });
-
-
-db.createCollection("transactions");
-db.transactions.insertOne({ txnID: "exampleTxnIdYY", clientID: "exampleClientId", stockID: "exampleStockId", quantity: 5, buySell: "buy", price: 150.60, timestamp: new Date(), lastUpdated: new Date() });

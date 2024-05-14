a73913bcb31cec06e783458064702f6aa1d5bf0a
Eliyahu Levy <eliyahu.levy21@gmail.com>
2024-05-13 20:37:02 -0400

    postgres startup

--- Distributed Trade Service/services/postgres/postgressetup.sql
+++ Distributed Trade Service/services/postgres/postgressetup.sql
@@ -1,15 +1,18 @@
+CREATE DATABASE tradeserver;
+
 \c tradeserver
+
 CREATE TABLE events (
     poolId VARCHAR(32),
-    eventID bigint,
-    machineId bigint,
+    eventID BIGINT,
+    machineId BIGINT,
+    -- eventType VARCHAR(32),
     eventKey VARCHAR(32),
-    eventValue real,
-    timestamp timestamp,
-    PRIMARY KEY (poolId,eventID,eventKey)
+    eventValue REAL,
+    timestamp TIMESTAMP,
+    -- PRIMARY KEY (poolId, eventID, eventKey, eventType)
+    PRIMARY KEY (poolId, eventID, eventKey)
 );
 
--- COPY events
---     FROM '/data/fakeevents.csv' 
---     DELIMITER ',' 
---     CSV HEADER;
+GRANT ALL PRIVILEGES ON DATABASE tradeserver TO myuser;
+GRANT ALL PRIVILEGES ON TABLE events TO myuser;
